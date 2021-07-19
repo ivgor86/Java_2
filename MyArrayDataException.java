0792f98a6a740956b12ccd6ad1b4ec5a7f3fd869
Ivgor86 <Ivgor86@mail.ru>
2021-07-19 20:51:04 +0300

    Lesson_2

--- MyArrayDataException.java
+++
@@ -1,7 +0,0 @@
-package Lesson_2;
-
-public class MyArrayDataException extends RuntimeException {
-    public MyArrayDataException(String message) {
-        super("Некорректные данные "+message);
-    }
-}
--- MyArraySizeException.java
+++
@@ -1,7 +0,0 @@
-package Lesson_2;
-
-public class MyArraySizeException extends RuntimeException{
-    public MyArraySizeException() {
-        super("Размер массива не корректный!");
-    }
-}
